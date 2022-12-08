243df02f510c0e4bef361abf1950fe3db8a8f4ae
swetha_555 <65771591+swethaogeti@users.noreply.github.com>
2022-12-08 15:12:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import "./styles/tailwind-pre-build.css";
+
+export default function App() {
+  return (
+    <div className="text-2xl">
+      <h2 className="bg-green-400">sweety</h2>
+    </div>
+  );
+}
