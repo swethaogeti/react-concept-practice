b8b1b5d940cfbf11dc2df3fb4bab95243dd804ca
swetha_555 <65771591+swethaogeti@users.noreply.github.com>
2022-12-21 17:54:04 +0530

    practice

--- src/concepts/managingState/ResetDetails.js
+++ src/concepts/managingState/ResetDetails.js
@@ -1,50 +1,65 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
 const initialContacts = [
-  { id: 0, name: "Taylor", email: "taylor@mail.com" },
-  { id: 1, name: "Alice", email: "alice@mail.com" },
-  { id: 2, name: "Bob", email: "bob@mail.com" }
+  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
+  { id: 1, name: 'Alice', email: 'alice@mail.com' },
+  { id: 2, name: 'Bob', email: 'bob@mail.com' }
 ];
-export const ResetDetails = () => {
-  const [details, setDetails] = useState(initialContacts[0]);
+export const ResetDetails=()=>{
+  const [contacts,setContacts]=useState(initialContacts)
+ const [selectedId,setSelectedId]=useState(0)
 
-  const handleChangeContact = (item) => {
-    setDetails(item);
-  };
+ const handleSelectId=(id)=>{
+   setSelectedId(id)
+ }
+//selectedContact is created to avoid dupliction of state
+  const selectedContact=contacts.find(c=>c.id===selectedId)
+ 
+
+  const handleSave=(updatedData)=>{
+    const nextContact=contacts.map(c=>{
+      if(c.id===updatedData.id){
+        return updatedData
+      }else{
+      return  c
+      }
+
+    })
+    setContacts(nextContact)
+  }
+  return(
+  <>
+  <h1>Reset details</h1>
+  <ContactList contacts={contacts} selectedId={selectedId} handleSelectId={handleSelectId}/>
+  <br/>
+  <hr/>
+  <Editcontacts key={selectedId} initialData={selectedContact} handleSave={handleSave}/>
+  </>)
+}
+
+const ContactList=({contacts,selectedId,handleSelectId})=>{
   return (
     <>
-      <ContactList
-        contacts={initialContacts}
-        handleChangeContact={handleChangeContact}
-      />
-      <br />
-      <EditContact contact={details} />
-    </>
-  );
-};
+    {contacts.map(item=> <button key={item.id} onClick={()=>handleSelectId(item.id)}>{item.id===selectedId?<b>{item.name}</b>:item.name}</button>)}
+   </>
+  )
+}
 
-const ContactList = ({ contacts, handleChangeContact }) => {
+const Editcontacts=({initialData,handleSave})=>{
+const[name,setName]=useState(initialData.name)
+const [email,setEmail]=useState(initialData.email)
+
   return (
     <>
-      {contacts.map((item) => (
-        <button onClick={() => handleChangeContact(item)}>{item.name}</button>
-      ))}
-    </>
-  );
-};
+    <span>name:</span>
+    <input value={name} onChange={e=>setName(e.target.value)}></input>
 
-const EditContact = ({ contact }) => {
-  return (
-    <>
-      <label>
-        Name:
-        <input value={contact.name}></input>
-      </label>
+    <span>Email:</span>
+    <input value={email} onChange={e=>setEmail(e.target.value)}></input>
+    <hr/>
+    <button onClick={()=>handleSave({id:initialData.id,name:name,email:email})}>Save</button>
+    <button onClick={()=>{setName(initialData.name);setEmail(initialData.email)}}>Reset</button>
 
-      <label>
-        Email:
-        <input value={contact.email}></input>
-      </label>
     </>
-  );
-};
+  )
+}
