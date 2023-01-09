bac7af2a3bd10457f33b96e34e317b2823fcdb7f
swetha_555 <65771591+swethaogeti@users.noreply.github.com>
2023-01-09 23:01:15 +0530

    redux toolkit

--- src/shopping-cart-redux/pages/ProductListing.js
+++ src/shopping-cart-redux/pages/ProductListing.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 
 export const ProductListing = () => {
   const { data } = useSelector((state) => state.products);
-  // console.log(data);
+  console.log(data);
   return (
     <>
       <h1>shopping cart using redux</h1>
